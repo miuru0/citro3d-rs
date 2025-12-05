6126997b6f8755cd76cb8b3b06b421a4fe42bf50
miuru0 <pacoelchicote@gmail.com>
2025-12-05 18:32:08 +0100

    Make Shape trait only accessible in the crate to avoid using it outside a render target

--- citro2d/src/shapes.rs
+++ citro2d/src/shapes.rs
@@ -11,7 +11,7 @@ pub struct MultiColor {
 }
 
 /// A trait to help render all 2D shapes supported by citro2d
-pub trait Shape {
+pub(crate) trait Shape {
     //TODO possibly return Option<self>.
     fn render(&self) -> bool;
 }
